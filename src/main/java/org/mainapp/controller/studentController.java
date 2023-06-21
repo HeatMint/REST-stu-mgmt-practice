8fb29cdf3dbb6e7bfa1067629bb58451998afe6d
Mingkai Lu <heatmint2333@gmail.com>
2023-06-21 14:26:08 +0800

    CRUD

--- src/main/java/org/mainapp/controller/studentController.java
+++ src/main/java/org/mainapp/controller/studentController.java
@@ -1,12 +1,10 @@
 package org.mainapp.controller;
 
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import org.mainapp.cls.Student;
 import org.mainapp.mapper.StudentMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.desktop.SystemEventListener;
 import java.lang.reflect.Array;
@@ -17,9 +15,10 @@ public class studentController {
     @Autowired
     private StudentMapper studentMapper;
     @GetMapping("/humanview")
-    public String hello(){
+    public String humanView(@RequestParam Map<String, Object> map){
         StringBuilder ret = new StringBuilder();
-        List<Student> stuList = studentMapper.selectList(null);
+        List<Student> stuList = studentMapper.selectByMap(map);
+
         for (int i = 0; i < stuList.size(); i++) {
             Student s = stuList.get(i);
             ret.append(s.toString()).append("<p>");
@@ -50,10 +49,23 @@ public class studentController {
 //            String value = map.get(key);
 //            m.put(key,value);
 //        }
-        //昨天脑子昏头了写这玩意出来
+        //昨天脑子昏头咋了写这玩意出来
 
         studentMapper.deleteByMap(map);
         return "Sucess!";
     }
 
+    @PostMapping(value = "/edit/{studentName}")
+    public String editStudent(@PathVariable String studentName, @RequestBody Map<String, String> map){
+        UpdateWrapper<Student> updateWrapper = new UpdateWrapper<>();
+        updateWrapper.eq("name", studentName);
+        for (String key:
+             map.keySet()) {
+            updateWrapper.set(key,map.get(key));
+        }
+        studentMapper.update(null,updateWrapper);
+        return "Sucess!";
+    }
+
 }
+
